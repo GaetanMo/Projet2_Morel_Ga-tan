0a8b09424b564a84d73e3ce2d9057ca2092ae42b
GaétanMo <gaetan.mo78@hotmail.fr>
2024-04-24 16:34:14 +0200

    Scrapping de la page d'un seul livre terminé

--- Projet2.py
+++ Projet2.py
@@ -2,12 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
 response = requests.get(url)
+response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text,"html.parser")
 
-
+#Url de la page
 product_page_url = url
 
 #Tableau "product description" qui contient certaines infos qui m'intéressent extraites dans lignes_tableau
@@ -26,28 +28,35 @@ title = soup.find("h1").text
 paragraphes = soup.find_all('p')
 product_description = paragraphes[3].text
 
-#Je scrap la catégorie du livre
+#Je scrap la catégorie du livre (et j'enleve des retours à la lignes inutiles)
 chemin = soup.find_all("li")
 category = chemin[2].text
+category = category.replace('\n', '')
 
-#Je scrap les avis (étoiles)
+#Je scrap les avis (étoiles), sauf qu'ils étaient dans une class et ça s'est enregistré sous forme de liste, j'ai donc dû utiliser .join pour qu'ils ne soient plus dans une liste
 paragraphe_avis = paragraphes[2]
-review_rating = paragraphe_avis['class']
+review_rating_list = paragraphe_avis['class']
+review_rating = ' : '.join(review_rating_list)
+review_rating = review_rating.replace('"', '')
+
+#Je scrap l'URL de l'image
+image_url_line = soup.find("img")
+image_url_src = image_url_line["src"]
+image_url_src = image_url_src.replace('../../', '') #J'enlève les ../../ en trop avant le "média"
+lien_general = "https://books.toscrape.com/"
+image_url = lien_general + image_url_src
+
 
 
 #liste dans l'ordre des informations à mettre dans le fichier csv
-liste_informations = [product_page_url, UPC, title, price_including_tax, price_excluding_tax, number_available,product_description, category, review_rating]
+liste_informations = [product_page_url, UPC, title, price_including_tax, price_excluding_tax, number_available,product_description, category, review_rating, image_url]
 
-print(liste_informations)
-
-
-
-""" Prototype de création du fichier csv
-
-en_tete = ['product_page_url', 'universal_ product_code (upc)', 'title, price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
+#Création fichier CSV
+en_tete = ['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
 
 with open('data.csv', 'w') as csv_file:
-writer = csv.writer(csv_file, delimiter=',')
-writer.writerow(en_tete)
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(en_tete)
+    writer.writerow(liste_informations)
 
-"""
+
