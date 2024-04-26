d4fb5dd4d118646f495cd15252805419e97e48f0
GaétanMo <gaetan.mo78@hotmail.fr>
2024-04-26 11:38:10 +0200

    étape 3 réussie, le main.py scrap toutes les données des produits de la catégorie sequential art

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-import scrap_un_livre
+from scrap_un_livre import scrap_un_livre
 
 
-def scrap_url_livres(url_page_actuelle, links):
+def scrap_url_livres(url_page_actuelle, stockage): #fonction qui permet de scrap les URL de tous les livres de la catégorie sequential-art
     response_category = requests.get(url_page_actuelle)
     response_category.encoding = 'utf-8'
     soup = BeautifulSoup(response_category.text, "html.parser")
@@ -18,23 +18,35 @@ def scrap_url_livres(url_page_actuelle, links):
     for div in divs:
         a = div.find('a')
         link = a['href'].replace('../../../', '')
-        links.append("https://books.toscrape.com/catalogue/" + link)
+        stockage.append("https://books.toscrape.com/catalogue/" + link)
 
     bouton_suivant = soup.find("li", class_="next")
-    if bouton_suivant:
+    if bouton_suivant: 
         page_suivante = bouton_suivant.find("a")["href"]
         page_suivante = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/" + page_suivante
-        scrap_url_livres(page_suivante, links)
+        scrap_url_livres(page_suivante, stockage)
 
 
-# Lancer la fonction pour visiter les pages
-url_category = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
+#Lance la fonction pour visiter la catégorie sequential art, et les stocker dans la liste links
+url_sequentialart = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
 links = []
-scrap_url_livres(url_category, links)
+scrap_url_livres(url_sequentialart, links)
 
-# Afficher les liens des livres collectés
-print(links)
+#Création de l'entête du fichier CSV
+en_tete = ['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
 
+with open('data.csv', 'w') as csv_file: #Création fichier CSV
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(en_tete) #Première ligne = en_tete
+    for link in links: #Pour tous les liens dans la liste links, on éxécute les deux lignes suivantes
+        informations_livre = scrap_un_livre(link)
+        writer.writerow(informations_livre)
+
+
+    
+    
+
+    
 
 
 
--- scrap_un_livre.py
+++ scrap_un_livre.py
@@ -4,68 +4,51 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def scrap_un_livre(url_book): #Fonction qui permet de scrap toutes les données nécessaires d'un livre à partir de l'URL de sa page
 
-url_book = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+    response = requests.get(url_book)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text,"html.parser")
 
-response = requests.get(url_book)
-response.encoding = 'utf-8'
-soup = BeautifulSoup(response.text,"html.parser")
+    #Url de la page
+    product_page_url = url_book
 
-#Url de la page
-product_page_url = url_book
+    #Tableau "product description" qui contient certaines infos qui m'intéressent extraites dans lignes_tableau
+    tableau = soup.find_all("td")
 
-#Tableau "product description" qui contient certaines infos qui m'intéressent extraites dans lignes_tableau
-tableau = soup.find_all("td")
+    #J'associe les valeurs du tableau aux variables demandées
+    UPC = tableau[0].text
+    price_including_tax = tableau[3].text
+    price_excluding_tax = tableau[2].text
+    number_available = tableau[5].text
 
-#J'associe les valeurs du tableau aux variables demandées
-UPC = tableau[0].text
-price_including_tax = tableau[3].text
-price_excluding_tax = tableau[2].text
-number_available = tableau[5].text
+    #Je scrap le titre du livre
+    title = soup.find("h1").text
 
-#Je scrap le titre du livre
-title = soup.find("h1").text
+    #Je scrap la description du livre
+    paragraphes = soup.find_all('p')
+    product_description = paragraphes[3].text
 
-#Je scrap la description du livre
-paragraphes = soup.find_all('p')
-product_description = paragraphes[3].text
+    #Je scrap la catégorie du livre (et j'enleve des retours à la lignes inutiles)
+    chemin = soup.find_all("li")
+    category = chemin[2].text
+    category = category.replace('\n', '')
 
-#Je scrap la catégorie du livre (et j'enleve des retours à la lignes inutiles)
-chemin = soup.find_all("li")
-category = chemin[2].text
-category = category.replace('\n', '')
+    #Je scrap les avis (étoiles), sauf qu'ils étaient dans une class et ça s'est enregistré sous forme de liste, j'ai donc dû utiliser .join pour qu'ils ne soient plus dans une liste
+    paragraphe_avis = paragraphes[2]
+    review_rating_list = paragraphe_avis['class']
+    review_rating = ' : '.join(review_rating_list)
+    review_rating = review_rating.replace('"', '')
 
-#Je scrap les avis (étoiles), sauf qu'ils étaient dans une class et ça s'est enregistré sous forme de liste, j'ai donc dû utiliser .join pour qu'ils ne soient plus dans une liste
-paragraphe_avis = paragraphes[2]
-review_rating_list = paragraphe_avis['class']
-review_rating = ' : '.join(review_rating_list)
-review_rating = review_rating.replace('"', '')
+    #Je scrap l'URL de l'image
+    image_url_line = soup.find("img")
+    image_url_src = image_url_line["src"]
+    image_url_src = image_url_src.replace('../../', '') #J'enlève les ../../ en trop avant le "média"
+    lien_general = "https://books.toscrape.com/"
+    image_url = lien_general + image_url_src
 
-#Je scrap l'URL de l'image
-image_url_line = soup.find("img")
-image_url_src = image_url_line["src"]
-image_url_src = image_url_src.replace('../../', '') #J'enlève les ../../ en trop avant le "média"
-lien_general = "https://books.toscrape.com/"
-image_url = lien_general + image_url_src
+    #liste dans l'ordre des informations à mettre dans le fichier csv
+    liste_informations = [product_page_url, UPC, title, price_including_tax, price_excluding_tax, number_available,product_description, category, review_rating, image_url]
 
+    return liste_informations
 
-
-#liste dans l'ordre des informations à mettre dans le fichier csv
-liste_informations = [product_page_url, UPC, title, price_including_tax, price_excluding_tax, number_available,product_description, category, review_rating, image_url]
-
-
-
-
-
-"""
-#Création fichier CSV
-
-en_tete = ['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
-
-#Pour ouvrir un fichier data.csv correspondant au cas où
-
-with open('data.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(en_tete)
-    writer.writerow(liste_informations)
-"""
